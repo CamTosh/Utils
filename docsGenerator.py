d7d5c62ddd4fd4a32d8136f8bdcbd6589398ab5c
Tosh Camille <tochecamille@gmail.com>
2016-08-27 17:17:25 +0200

    Get docstring

--- docsGenerator.py
+++ docsGenerator.py
@@ -3,50 +3,49 @@ import re
 class Docs(object):
 	"""Get class name and docstring for generate Docs"""
 
-	def __init__(self, fichier):
-		self.file = open(fichier, 'r')
+	def __init__(self, f):
+		self.file = open(f, 'r')
 
 		self.listeClasses = []
 		self.listeDocString = []
 		self.listeLibrairies = []
 
-		self.getClass()
-		self.getDocString()
-		self.getLib()
-
-
-	def __str__(self):
-		# Todo
-		documentation = {}
-		
-		for k, c in self.listeClasses:
-			documentation[c] = self.listeDocString[k]
-
-		return documentation
-
 
 	def getClass(self):
-
+				
 		for f in self.file:
 			l = re.search('(class)(.*)', f)
 			
-			if str(l) != "None":
+			if l:
 				s = l.group(0)
 				className = s.replace(' ', '').replace("class", '').split("(")
 				self.listeClasses.append(className[0])
 
+		return self.listeClasses
 
-	def getDocString(self):
-		# Todo
+
+	def getDoc(self):
+		
 		for f in self.file:
-			l = re.search('(class)[^$](.*)', f)
+			l = re.search('"""(.*?)"""', f)
+			
+			if l:
+				s = l.group(0)
+				doc = s.replace('"""', ' ')
+				self.listeDocString.append(doc)
+
+		return self.listeDocString
 
 
-	def getLib(self);
-		# Todo
-		for f in file:
+	def getLib(self):
+		
+		for f in self.file:
+
 			if f.startswith('import'):
 				self.listeLibrairies.append(f.replace("import", "").replace(' ', ""))
+
 			if f.startswith('from'):
 				rer = f.split("import")
-				self.listeLibrairies.append(rer[1].replace("import", "").replace(' ', ""))
+				self.listeLibrairies.append(rer[1].replace("import", "").replace(' ', "").replace("\n", ""))
+
+		return self.listeLibrairies
